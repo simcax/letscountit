b34463bc7b91b018236d6fa14de71b941e4fe916
MiniMe <christian@crownie.dk>
2022-05-25 21:46:18 +0200

    Cleanup in test_base code

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,41 +1,48 @@
 import pytest
 import letscountit.base
-from letscountit.base import counterthing
+from letscountit.base import Counterthing
 import uuid
 
+
 def test_class_loads():  
-    x = letscountit.base.basecounting()
+    x = letscountit.base.Basecounting()
     from types import ModuleType as MT
-    all = [k for k,v in globals().items() if type(v) is MT and not k.startswith('__')]
+    all = [k for k, v in globals().items() if type(v) is MT and not k.startswith('__')]
     ", ".join(all)
     assert 'letscountit' in all
 
+
 def test_get_count_obj():
     uid = uuid.uuid4()
-    count_obj = counterthing(uid)
+    counterthing1 = Counterthing(uid)
+    count_obj = counterthing1
     assert count_obj.uuid == uid
+
 
 def test_increase_counter_on_counterthing():
     uid = uuid.uuid4()
-    count_obj = counterthing(uid)
+    count_obj = Counterthing(uid)
     count_obj.up()
     assert count_obj.count == 1
 
+
 def test_increase_counter_with_ten_on_counterthing():
     uid = uuid.uuid4()
-    count_obj = counterthing(uid)
+    count_obj = Counterthing(uid)
     for i in range(10):
         count_obj.up()
     assert count_obj.count == 10
 
+
 def test_increase_counter_with_ten_on_counterthing_by_number():
     uid = uuid.uuid4()
-    count_obj = counterthing(uid)
+    count_obj = Counterthing(uid)
     count_obj.up(10)
     assert count_obj.count == 10
 
+
 def test_increase_counter_with_ten_on_counterthing_exception():
     uid = uuid.uuid4()
-    count_obj = counterthing(uid)
+    count_obj = Counterthing(uid)
     with pytest.raises(Exception):
         count_obj.up('a')
